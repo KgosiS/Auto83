c2737f896555fd9eea93182d79770321515a7f95
Kgosi Sebako <139534118+KgosiS@users.noreply.github.com>
2026-09-18 09:11:36 +0000

    modified:   z83-template.js

--- z83-template.js
+++ z83-template.js
@@ -51,8 +51,8 @@ window.createOfficialZ83Document = async (profile = {}) => {
   setText('Department where the position was advertised', additional.department);
   setText('Reference number as stated in the advert', additional.referenceNumber);
   setText('If you are offered the position when can you start OR how much notice must you serve with your current employer', additional.startDate ? date(additional.startDate) : additional.noticePeriod);
-  setText('Surname and Full names', fullName);
-  setText('Surname and Full names_2', lastName);
+  setText('Surname and Full names', lastName);
+  setText('Surname and Full names_2', firstName);
   setText('DDMMYY', date(personal.dob));
   setText('Identity Number', profile.idNumber || personal.idNumber);
   setText('Passport2 number', personal.passportNumber);
